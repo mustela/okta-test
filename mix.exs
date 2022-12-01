30cc3fd79bffa40c1e7688a28c6e63b1ab47f0c6
mustela <ejankowski@gmail.com>
2022-12-01 16:29:35 +0100

    testing branch

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule OktaTest.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:ueberauth, "~> 0.10.3"},
-      {:ueberauth_okta, "~> 1.0"}
+      {:ueberauth_okta,
+       git: "https://github.com/jjcarstens/ueberauth_okta", branch: "fix-runtime"}
     ]
   end
 
